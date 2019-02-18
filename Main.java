526c5bff4061ba1cf49b4373072711d7c8ffb9d7
Dimitar Tsvetkov <tsvetkovdimitar@gmai.com>
2019-02-18 16:21:49 +0000

    Bulls and Cows game SoftUni-TechModule(Problem solving lecture

---
+++ Main.java
@@ -0,0 +1,14 @@
+package bullsandcows;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        BullsAndCows app = new BullsAndCows();
+        app.createAppForm();
+
+        app.setVisible(true);
+
+    }
+
+}
